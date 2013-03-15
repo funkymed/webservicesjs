24a8f4f2fd3c79e0afc99e130c6aee8795d48d6a
Cyril <cyril.pereira@gmail.com>
2013-03-15 17:16:51 +0800

    proxy file comment

--- proxy.php
+++ proxy.php
@@ -1,39 +1,43 @@
 <?php
-    $url    = '';
-    $method = '';
-    $vars   = array();
+/**
+ * Proxy to load file or webservice in ajax call
+ * Just call this file with a varirable called 'u'  who contain the url
+ */
+$url    = '';
+$method = '';
+$vars   = array();
 
-    if(isset($_GET['u']))
-    {
-        $url = $_GET['u'];
-        unset($_GET['u']);
-        $vars = $_GET;
-        $method = 'GET';
-    }else if(isset($_POST['u']))
-    {
-        $url = $_POST['u'];
-        unset($_POST['u']);
-        $vars = $_POST;
-        $method = 'POST';
-    }
+if(isset($_GET['u']))
+{
+    $url = $_GET['u'];
+    unset($_GET['u']);
+    $vars = $_GET;
+    $method = 'GET';
+}else if(isset($_POST['u']))
+{
+    $url = $_POST['u'];
+    unset($_POST['u']);
+    $vars = $_POST;
+    $method = 'POST';
+}
 
-    if($url!='')
+if($url!='')
+{
+    if($method=='POST')
     {
-        if($method=='POST')
-        {
-            $postdata = http_build_query($vars);
-            $opts = array('http' =>
-                array(
-                    'method'  => $method,
-                    'header'  => 'Content-type: application/x-www-form-urlencoded',
-                    'content' => $postdata
-                )
-            );
-            $context  = stream_context_create($opts);
-            print file_get_contents($url, false, $context);
-        }else{
-            $arr = array();
-            foreach($vars as $k=>$v) $arr[]=$k.'='.$v;
-            print file_get_contents($url.'?'.implode('&',$arr));
-        }
+        $postdata = http_build_query($vars);
+        $opts = array('http' =>
+                      array(
+                          'method'  => $method,
+                          'header'  => 'Content-type: application/x-www-form-urlencoded',
+                          'content' => $postdata
+                      )
+        );
+        $context  = stream_context_create($opts);
+        print file_get_contents($url, false, $context);
+    }else{
+        $arr = array();
+        foreach($vars as $k=>$v) $arr[]=$k.'='.$v;
+        print file_get_contents($url.'?'.implode('&',$arr));
     }
+}
